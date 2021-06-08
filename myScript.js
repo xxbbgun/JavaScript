ab6bfa597031835ba0df07182b9cc9ede3beddaa
xxbbgun <Kanticha_sr@cmu.ac.th>
2021-06-08 15:53:02 +0700

    commit 2

--- myScript.js
+++ myScript.js
@@ -6,3 +6,20 @@ document.getElementById('ok').addEventListener('click',function(e){
     document.getElementById('no').style.marginLeft = `${marginBox}px`
     document.getElementById('no').style.marginRight = `${marginBox}px`
 })
+
+
+
+
+
+
+var text = ['ok','วิชานี้','ง่าย','จริงๆนะ']
+var count = 0
+var newText =document.getElementById('no')
+newText.addEventListener('dblclick',function(e){
+ if(count>3){
+     count = 0
+ }else{
+     document.getElementById('ok').innerHTML = text[count]
+     count++
+ }
+})
